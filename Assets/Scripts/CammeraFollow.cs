378c3fffe0923623d5cddbeef1dafc7c143857e2
ludomirc <beniamin.czaplicki@gmail.com>
2019-01-08 11:11:29 +0100

    add character tracking via a camera

---
+++ Assets/Scripts/CammeraFollow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Tiled2Unity;
+using UnityEngine;
+
+public class CammeraFollow : MonoBehaviour
+{
+
+	private Vector2 valocity;
+	
+	public float smoothTimeY;
+	public float smoothTimeX;
+	
+	public GameObject player;
+	
+	void Start ()
+	{
+		
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+
+	void FixedUpdate ()
+	{
+		Debug.Log("FixedUpdate time :" + Time.deltaTime);
+		float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref valocity.x, smoothTimeX);
+		float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y, ref valocity.y, smoothTimeY);
+		
+		transform.position = new Vector3(posX,posY,transform.position.z);
+	}
+}
