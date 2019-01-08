a711b57a4d45f471c963eb8bf819641827afc0b7
ludomirc <beniamin.czaplicki@gmail.com>
2019-01-08 09:01:01 +0100

    added winnin zonne and new scene stub

---
+++ Assets/Scripts/WinningZone.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Collider2D))]
+public class WinningZone : MonoBehaviour
+{
+
+
+	[SerializeField] private float timeDelay = 3.0f;
+	[SerializeField] private  GameObject  Winning;
+	[SerializeField] private string nextLevelToLoad = "Scene02";
+
+
+	private IEnumerator loadNextLevel()
+	{
+		Winning.SetActive(true);
+		yield return  new WaitForSeconds(timeDelay);
+		
+		SceneManager.LoadScene(nextLevelToLoad);
+	}
+	
+	public void OnTriggerEnter2D(Collider2D collision) {
+		
+		if (collision.gameObject.CompareTag("Player"))
+		{
+			StartCoroutine(loadNextLevel());
+		}
+	}
+}
